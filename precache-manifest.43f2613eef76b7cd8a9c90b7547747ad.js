8823aded69585ddc2bc846f1d5b289de06c6ae29
Leonidas Esteban <leonidasesteban@gmail.com>
2020-05-23 21:27:06 -0500

    Updates

---
+++ precache-manifest.43f2613eef76b7cd8a9c90b7547747ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21a8ac147d6de99bc636065b907fabef",
+    "url": "/rock-paper-scissors-react/index.html"
+  },
+  {
+    "revision": "feb6eb3c43b469292d8d",
+    "url": "/rock-paper-scissors-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c0c93c65e7b02028395c",
+    "url": "/rock-paper-scissors-react/static/js/2.e206e3cc.chunk.js"
+  },
+  {
+    "revision": "b92b369c485a56d25a5a7495ecf46b59",
+    "url": "/rock-paper-scissors-react/static/js/2.e206e3cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feb6eb3c43b469292d8d",
+    "url": "/rock-paper-scissors-react/static/js/main.547d2cbb.chunk.js"
+  },
+  {
+    "revision": "16989899433d5a8dc264",
+    "url": "/rock-paper-scissors-react/static/js/runtime-main.6b24b7d1.js"
+  }
+]);
